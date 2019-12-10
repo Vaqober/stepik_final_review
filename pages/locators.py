1db8769bbb5291d9e58244505ca22cb6a56204a0
vaqober <vaqober@mail.ru>
2019-12-10 18:43:13 +0500

    add start to main page tests

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,4 @@ class LoginPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.XPATH, "//div[contains(@class, 'basket-mini')]//a")
--- test_main_page.py
+++ test_main_page.py
@@ -6,8 +6,14 @@ import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    main_page = MainPage(browser, link)
+    main_page.open()
+
